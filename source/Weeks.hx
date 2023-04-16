b80fbd8ded4dcae23258548b4267a1637b8c91b1
ronbrazy <ronnnyboi765@gmail.com>
2023-04-16 04:28:30 -0400

    erm

--- source/Weeks.hx
+++ source/Weeks.hx
@@ -5,11 +5,11 @@ class Weeks
     public static var weeks:Array<Dynamic> = [
 		['puffball', 'thomas', [146,113,253]],
 		['flying-kipper', 'henry', [146,113,253]],
-    ['confusion-and-delay','fatass',[146,113,253]],
     ['splendid','james',[146,113,253]],
+    ['indignation','gordon',[146,113,253]],
+    ['confusion-and-delay','fatass',[146,113,253]],
     ['sad-story','henry',[146,113,253]],
     ['endless-remix','thomas',[146,113,253]],
-    ['indignation','gordon',[146,113,253]],
     ['godrays-remix','gordon',[146,113,253]],
     ];
 }
