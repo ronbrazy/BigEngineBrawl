c438c84def328065195b9a69debfd4b167aa721b
ronbrazy <ronnnyboi765@gmail.com>
2023-05-06 22:26:52 -0400

    more song stuffs, updated ratings

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -86,16 +86,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Scrapped', 0.2], //From 0% to 19%
+		['Rubbish', 0.4], //From 20% to 39%
+		['Troublesome', 0.5], //From 40% to 49%
+		['Lazybones', 0.6], //From 50% to 59%
+		['Pah!', 0.69], //From 60% to 68%
+		['Reliable', 0.7], //69%
+		['Splendid', 0.8], //From 70% to 79%
+		['Grand', 0.9], //From 80% to 89%
+		['Really Useful!', 1], //From 90% to 99%
+		['Really Useful!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	//event variables
--- source/Weeks.hx
+++ source/Weeks.hx
@@ -10,7 +10,10 @@ class Weeks
     ['confusion-and-delay','fatass',[146,113,253]],
     ['sad-story','sadhenry',[146,113,253]],
     ['endless-remix','thomas',[146,113,253]],
+    ['monochrome-remix','sadhenry',[146,113,253]],
     ['ugh-remix','james',[146,113,253]],
     ['godrays-remix','gordon',[146,113,253]],
+    ['loathed','alfred',[146,113,253]],
+    ['old-reliable','edward',[146,113,253]],
     ];
 }
